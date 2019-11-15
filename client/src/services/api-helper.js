2408ff4ad370c04ef7939053077f50810c447d8e
Melanie Phan <melanie.phan@prudential.com>
2019-11-14 19:14:13 -0500

    front end

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://enigmatic-savannah-82227.herokuapp.com"
 })
 
 // ============== Auth ================
