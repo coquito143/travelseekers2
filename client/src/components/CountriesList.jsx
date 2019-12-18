536b1825545c4cf91054ba775d711bac713df509
Melanie Phan <melanie.phan@prudential.com>
2019-12-18 11:39:29 -0500

    changed image hover effect

--- client/src/components/CountriesList.jsx
+++ client/src/components/CountriesList.jsx
@@ -6,22 +6,26 @@ export default function CountriesList(props) {
   return (
     <div id="countries-div">
       <div id="countries-div-heading">
-      <div id="view-countries-text1"><h2>Countries</h2></div>
-                  <div id="view-countries-text2">
-        <span><h2>where the dollar goes farthest</h2></span>
-   
+        <div id="view-countries-text1"><h2>Countries</h2></div>
+        <div id="view-countries-text2">
+          <span><h2>where the dollar goes farthest</h2></span>
+
         </div>
-        </div>
+      </div>
       <div className="countries-list-div">
         {props.countries.map(country => (
-        <React.Fragment key={country.id}>
+          <React.Fragment key={country.id}>
+            <div className="country-wrapper-div">
             <Link to={`/countries/${country.id}`}>
-              <img className="country-img" src={country.image_url}/>
+              <div className="countries-img">
+                <img className="country-img" src={country.image_url} />
+              </div>
               <h3>{country.country_name}</h3>
             </Link>
-        </React.Fragment>
+            </div>
+          </React.Fragment>
         ))}
-        </div>
+      </div>
     </div>
   )
 }
